f9aa01df1c0629a33c69a6e04cc65a2d427ebabf
1582130940 <1582130940@qq.com>
2020-02-07 16:23:03 +0800

    gms: Remove GoogleTTS

--- config.mk
+++ config.mk
@@ -4,7 +4,6 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 PRODUCT_PACKAGES += \
     CalculatorGooglePrebuilt \
     CalendarGooglePrebuilt \
-    GoogleTTS \
     LatinIMEGooglePrebuilt \
     PrebuiltDeskClockGoogle \
     PrebuiltExchange3Google \
